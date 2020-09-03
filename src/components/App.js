dd137f2caad23a7b893371c538d066b7b46b69cd
Roshan Thomas <roshan.thomas@qatar.tamu.edu>
2020-09-03 15:00:25 +0300

    corrected signup routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Login from "./login/Login";
-import Signup from "./signup/Signup";
+import Signup1 from "./signup-1/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signup/1" component={Signup1} />
       </Switch>
     </BrowserRouter>
   );
